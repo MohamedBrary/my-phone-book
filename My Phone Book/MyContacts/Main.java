13d818fcc9f3d7faee0eb6602db231f3bcac05d9
Mohamed Brary <mohamedbrary@gmail.com>
2017-04-24 16:03:48 +0200

    initial commit

---
+++ My Phone Book/MyContacts/Main.java
@@ -0,0 +1,16 @@
+package MyContacts;
+/**
+ * This program represent a digital phone book , 
+ * you can create groups of contacts  , 
+ * add, modify ,delete , search for contacts .
+ * 
+ * @author Mohamed Brary
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args)  {	
+		
+		Method.getMenus();
+	}
+}
